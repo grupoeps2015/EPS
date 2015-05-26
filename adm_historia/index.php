ce15fa21617565112e0b8f3a4e1ef8a86e825833
marlen2610 <marlen_2610@hotmail.com>
2015-05-26 14:55:51 -0600

    Control académico

--- adm_historia/index.php
+++ adm_historia/index.php
@@ -1,55 +1,85 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html>
+<!DOCTYPE html>
 
-<html xmlns="http://www.w3.org/1999/xhtml">
-	<head>
-        <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-        <title>Control académico - Escuela de Historia</title>
-		<link href="../css/login-box-adm_historia.css" rel="stylesheet" type="text/css" />
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Sistema de control academico - USAC</title>
+
+        <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css" type="text/css">
+        <link rel="stylesheet" href="../css/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="../css/creative.css" type="text/css">
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/jquery.easing.min.js"></script>
+        <script src="../js/jquery.fittext.js"></script>
+        <script src="../js/wow.min.js"></script>
+        <script src="../js/creative.js"></script>
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
     </head>
-	
-    <body> 
-        <div id="caja" style="width: 100%;"> 
-			<p align="center"><img class="alineado" src="../img/img10.png" width="auto" height="100px" align="left" />
-                <font style="font-size: 40px;">Control Acad&eacute;mico - Escuela de Historia</font>
-                <img class="alineado" src="../img/LogoEscuelaHistoria.png" width="auto" height="100px" align="right"  style="margin-right: 22px; "/>
-			</p>
-		 </div>
-		 
-		 <div style="padding: 100px 0 0 550px;">
-			<div id="caja" style="height: 415px;">
-				<div id="login-box">
-					<H2>Administradores</H2>
-					<br />
-                    <br />
-                    <div id="login-box-name" style="margin-top:20px;">
-						Usuario:
-					</div>
-					<div id="login-box-field" style="margin-top:20px;">
-						<input name="q" class="form-login" title="Usuario" value="" size="30" maxlength="2048" />
-					</div>
-                    <div id="login-box-name">
-						Password:
-					</div>
-					<div id="login-box-field">
-						<input name="q" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" />
-					</div>
-                    <br />
-                    <br />
-                    <a href="#"><img src="../img/login-btn.png" width="103" height="42" style="margin-left:90px;" /></a>
-				</div>
-			</div>
-		</div>
-	</body>
+
+    <body id="page-top">
+        <nav id="mainNav" class="navbar navbar-default navbar-fixed-top">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <img src="../img/img10.png" width="100px" height="50px" align="left" />
+                    <a class="navbar-brand page-scroll" href="#page-top" style="margin-left: 10px; ">Escuela de Historia - Control Acad&eacute;mico</a>
+                </div>
+
+                <!-- Menu superior lateral derecho -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav navbar-right">
+                        
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <header style="background-image: url(../img/fuente.jpg);">
+            <div class="header-content">
+                <br/> <br/>
+                
+                <div class="header-content-inner">
+                    <h2>Administradores</h2>
+                    <hr />
+                    <div class="row">
+                        <form class="col-md-4 col-md-offset-4 col-xs-6 col-xs-offset-3">
+                            <div class="form-group">
+                                <input type="text" class="form-control input-lg" placeholder="Usuario">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control input-lg" placeholder="Password">
+                            </div>
+                            <div class="form-group">
+                                <button class="btn btn-danger btn-lg btn-block">Iniciar Sesi&oacute;n</button>
+                                <br/><br/>
+                                <span class="pull-right"><a href="#">Olvidaste tu password?</a></span>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </header>
+        
+    </body>
+
 </html>
 
 
-
-
-
-
-
-
-
-
-</body>
-</html>
---
+++ adm_historia/inicio.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+        <title>Escuela de Historia</title>
+        
+        <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800' rel='stylesheet' type='text/css'>
+        <link href='http://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="../font-awesome/css/font-awesome.min.css" type="text/css">
+        <link rel="stylesheet" href="../css/animate.min.css" type="text/css">
+        <link rel="stylesheet" href="../css/creative.css" type="text/css">
+        <script src="../js/jquery.js"></script>
+        <script src="../js/bootstrap.min.js"></script>
+        <script src="../js/jquery.easing.min.js"></script>
+        <script src="../js/jquery.fittext.js"></script>
+        <script src="../js/wow.min.js"></script>
+        <script src="../js/creative.js"></script>
+        <!--[if lt IE 9]>
+            <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+            <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+        <![endif]-->
+
+    </head>
+
+    <body id="page-top">
+        <iframe scrolling="auto" src="encabezado.php" frameborder="0" height="50" width="100%"></iframe>
+
+        <section id="" style="background-color: beige;">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12 text-center">
+                        <h2 class="section-heading">Bienvenido</h2>
+                        <hr class="primary">
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-user wow bounceIn text-primary"></i>
+                            <h3>Gesti&oacute;n de Usuarios</h3>
+                            <p class="text-muted">Alumnos y catedráticos</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-book wow bounceIn text-primary"></i>
+                            <h3>Gesti&oacute;n de Cursos</h3>
+                            <p class="text-muted">Faculades, Escuelas y Centros Regionales</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-bar-chart wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                            <h3>Reportería</h3>
+                            <p class="text-muted">Directores, Control Academico, Catedraticos</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-pencil wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                            <h3>Gesti&oacute;n de estudiantes</h3>
+                            <p class="text-muted">Alumnos regulares</p>
+                        </div>
+                    </div>
+                </div>
+				                <div class="row">
+                
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-calendar wow bounceIn text-primary"></i>
+                            <h3>Gesti&oacute;n de asignaciones</h3>
+                            <p class="text-muted">Capacidad de salones y gestion de uso</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-book wow bounceIn text-primary"></i>
+                            <h3>Gesti&oacute;n de Unidades Acad&eacute;micas</h3>
+                            <p class="text-muted">Faculades, Escuelas y Centros Regionales</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-bar-chart wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                            <h3>Gesti&oacute;n de personal</h3>
+                            <p class="text-muted">Directores, Control Academico, Catedraticos</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="service-box">
+                            <i class="fa fa-4x fa-gear wow bounceIn text-primary" data-wow-delay=".2s"></i>
+                            <h3>Configuración</h3>
+                            <p class="text-muted">Otras configuraciones del sistema</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </body>
+
+</html>
