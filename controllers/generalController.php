c1c48db318f64144cad38f8c30a097e748df4e45
marlen2610 <marlen_2610@hotmail.com>
2015-09-17 00:54:46 -0600

    centro_unidad por sesion

--- controllers/generalController.php
+++ controllers/generalController.php
@@ -28,6 +28,10 @@ class generalController extends Controller{
         }
         session_start();
         if (isset($_SESSION["rol"]) && $_SESSION["rol"] == ROL_ADMINISTRADOR){
+            if($this->getInteger('hdCentroUnidad')){
+                $_SESSION["centrounidad"] = $this->getInteger('hdCentroUnidad');
+                $this->redireccionar($url);
+            }
             $lstCentros = $this->_ajax->getCentro();
             if(is_array($lstCentros)){
                 $this->_view->lstCentros = $lstCentros;
@@ -37,12 +41,31 @@ class generalController extends Controller{
             }
             
             $this->_view->titulo = 'Seleccionar Centro y Unidad - ' . APP_TITULO;
-            $this->_view->url = $url;
+            $this->_view->url = 'general/seleccionarCentroUnidad/'.$url;
             $this->_view->setJs(array('seleccionarCentroUnidad'));
             $this->_view->renderizar('seleccionarCentroUnidad');
         }
         else {
             $this->redireccionar($url);
+        }
+    }
+    
+    public function seleccionarCarreraEstudiante(){
+        if($this->getInteger('slCarreras')){
+            
+        }
+        else{
+            session_start();
+            $lstCentros = $this->_ajax->getCentro();
+            if(is_array($lstCentros)){
+                $this->_view->lstCentros = $lstCentros;
+            }else{
+                $this->redireccionar("error/sql/" . $lstCentros);
+                exit;
+            }
+            $this->_view->titulo = 'Seleccionar Carrera - ' . APP_TITULO;
+            $this->_view->setJs(array('seleccionarCarreraEstudiante'));
+            $this->_view->renderizar('seleccionarCarreraEstudiante');
         }
     }
     
