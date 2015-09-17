ba7a60083b54171fac3a7c8919c83ac8be1c7f17
rickshark130 <rick.shark130@gmail.com>
2015-09-17 15:02:38 -0600

    agregado requisitos de password

--- views/gestionUsuario/js/validarUsuario.js
+++ views/gestionUsuario/js/validarUsuario.js
@@ -1,4 +1,61 @@
 $(document).ready(function(){
+    var passValido = false;
+    
+    $('#txtPasswordNuevo')
+        .keyup(function() {
+            var pswd = $('#txtPasswordNuevo').val();
+            var bltotal = false;
+            var blletra = false;
+            var blmayus = false;
+            var blnum = false;
+            //verifica longitud
+            if ( pswd.length < 8 ) {
+                $('#total').removeClass('passValid').addClass('passInvalid');
+                bltotal=true;
+            } else {
+                $('#total').removeClass('passInvalid').addClass('passValid');
+                bltotal=false;
+            }
+            
+            //verifica que tenga una letra
+            if ( pswd.match(/[A-z]/) ) {
+                $('#letra').removeClass('passInvalid').addClass('passValid');
+                blletra=true;
+            } else {
+                $('#letra').removeClass('passValid').addClass('passInvalid');
+                blletra = false;
+            }
+            
+            //verifica que tenga una mayuscula
+            if ( pswd.match(/[A-Z]/) ) {
+                $('#mayus').removeClass('passInvalid').addClass('passValid');
+                blmayus=true;
+            } else {
+                $('#mayus').removeClass('passValid').addClass('passInvalid');
+                blmayus=false;
+            }
+            
+            //verifica que tenga un numero
+            if ( pswd.match(/\d/) ) {
+                $('#numero').removeClass('passInvalid').addClass('passValid');
+                blnum=true;
+            } else {
+                $('#numero').removeClass('passValid').addClass('passInvalid');
+                blnum= false;
+            }
+            
+            if(bltotal && blletra && blmayus && blnum){
+                passValido = true;
+            }else{
+                passValido = false;
+            }
+        })
+        .focus(function() {
+            $('#passInfo').show();
+        })
+        .blur(function() {
+            $('#passInfo').hide();
+        });
     
     $('#frmValidar').validate({
         rules:{
@@ -74,11 +131,15 @@ $(document).ready(function(){
         if(clave1 === "" || clave2 === ""){
             spErrorClave.innerText = "Debe ingresar una clave de acceso y confirmarla";
         }else{
-            if(clave1 !== clave2){
-                spErrorClave.innerText = "Las claves ingresadas no coinciden";
+            if(passValido){
+                if(clave1 !== clave2){
+                    spErrorClave.innerText = "Las claves ingresadas no coinciden";
+                }else{
+                    spErrorClave.innerText = "";
+                    claveAceptada = true;
+                }
             }else{
-                spErrorClave.innerText = "";
-                claveAceptada = true;
+                spErrorClave.innerText = "La clave no cumple con todos los requisitos";
             }
         }
         
