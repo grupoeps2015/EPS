19ba01992e977d719064a2b0f19ef5da32354e4a
rickshark130 <rick.shark130@gmail.com>
2015-09-20 00:50:10 -0600

    actualizadas las tablas para mostrar opciones e informacion en espaniol

--- views/gestionCentroUnidad/js/listadoUnidades.js
+++ views/gestionCentroUnidad/js/listadoUnidades.js
@@ -1,4 +1,4 @@
-$(document).ready( function () {
+$(document).ready(function(){
     $('#slExistentes').change(function(){
         if($('#slExistentes').val() === "NULL"){
             $('#btnAgregar').prop("disabled",true);
@@ -15,11 +15,18 @@ $(document).ready( function () {
         }
     });
     
-    $('#tbUnidadesAcademicas').DataTable( {
-        language: {
+    $('#tbUnidadesAcademicas').DataTable({
+        language:{
             emptyTable: "No hay informaci&oacute;n disponible.",
-            sZeroRecords: "No se encontro informaci&oacute;n compatible con la busqueda"
+            sZeroRecords: "No se encontro informaci&oacute;n compatible con la busqueda",
+            info: "Se muestran del _START_ al _END_ de _TOTAL_ registros",
+            infoEmpty: "No hay registros que mostrar",
+            paginate:{
+                next: "Siguiente",
+                previous: "Anterior"
+            },
+            search: "Buscar:",
+            lengthMenu: "Mostrar _MENU_ registros"
         }
-    } );
-    
-} );
+    });   
+});
--- views/gestionPensum/js/gestionCarrera.js
+++ views/gestionPensum/js/gestionCarrera.js
@@ -1,9 +1,18 @@
-$(document).ready( function () {
-    $('#tbCarreras').DataTable( {
-        "language": {
-          "emptyTable": "No hay informaci&oacute;n disponible."
+$(document).ready(function(){
+    $('#tbCarreras').DataTable({
+        language:{
+            emptyTable: "No hay informaci&oacute;n disponible.",
+            sZeroRecords: "No se encontro informaci&oacute;n compatible con la busqueda",
+            info: "Se muestran del _START_ al _END_ de _TOTAL_ registros",
+            infoEmpty: "No hay registros que mostrar",
+            paginate:{
+                next: "Siguiente",
+                previous: "Anterior"
+            },
+            search: "Buscar:",
+            lengthMenu: "Mostrar _MENU_ registros"
         }
-    } );
+    });
     
     $('#linkNuevoCar').click(function(){
        $('#frmPost').submit();
