f5817d46c918f4366c8cd94ff78e4a062c456f5b
GersonXicon <gersonxicon@gmail.com>
2015-10-27 01:14:38 -0600

    Consulta para obtener alumnos asignados a secciones de un profesor.

--- protected/Scripts/[SP]Catedratico.sql
+++ protected/Scripts/[SP]Catedratico.sql
@@ -95,5 +95,27 @@ $BODY$
   ROWS 1000;
 ALTER FUNCTION spinformacioncatedratico(integer)
   OWNER TO postgres;
+  
+/*
+SELECT distinct a.Asignacion, s.Seccion, s.Nombre as NombreSeccion, e.Carnet, e.PrimerNombre, e.SegundoNombre, e.PrimerApellido, e.SegundoApellido, tp.TipoPeriodo, tp.Nombre as NombreTipoPeriodo, c.Ciclo, c.Anio, tc.TipoCiclo, c.NumeroCiclo, tc.Nombre
+FROM EST_CUR_Asignacion a
+JOIN CUR_Seccion s ON s.Seccion = a.Seccion
+JOIN EST_Ciclo_Asignacion ca ON ca.Ciclo_Asignacion = a.Ciclo_Asignacion
+JOIN EST_Estudiante e ON e.Estudiante = ca.Estudiante
+JOIN ADM_Periodo p ON ca.Periodo = p.Periodo
+JOIN ADM_TipoPeriodo tp ON tp.TipoPeriodo = p.TipoPeriodo
+JOIN CUR_Ciclo c ON c.Ciclo = p.Ciclo
+JOIN CUR_TipoCiclo tc ON tc.TipoCiclo = c.TipoCiclo
+JOIN CUR_Trama t ON t.Seccion = a.Seccion
+JOIN CUR_Curso_Catedratico cc ON cc.Curso_Catedratico = t.Curso_Catedratico
+JOIN CAT_Catedratico cat ON cat.Catedratico = cc.Catedratico
+WHERE cat.Catedratico = 1
+AND tp.TipoPeriodo = 1
+AND c.Ciclo = 2
+AND tc.TipoCiclo = 1
+AND c.NumeroCiclo = 2
+*/
+
+
 
 Select 'Script de Catedraticos Instalado' as "Catedraticos";
