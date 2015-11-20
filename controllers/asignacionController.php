8278cedf85a12ae18ae33aadb601e7246031060a
marlen2610 <marlen_2610@hotmail.com>
2015-11-20 00:43:49 -0600

    manejo de error de versión de php

--- controllers/asignacionController.php
+++ controllers/asignacionController.php
@@ -871,6 +871,17 @@ class asignacionController extends Controller{
             exit;
         }
     }
+    
+    
 }
 
+    if(!function_exists("array_column")){
+
+        function array_column($array,$column_name){
+
+            return array_map(function($element) use($column_name){return $element[$column_name];}, $array);
+
+        }
+
+    }
 ?>
